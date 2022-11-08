d1ed32f459b60347a8d28d1b641e9759312b9249
Matthew Pachapa <90224964+mpachapa@users.noreply.github.com>
2022-11-08 14:23:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ streamlit.title('My Title')
 
 streamlit.header('My first streamlit project')
 streamlit.text('I first created a repo, and I then connected the repo to streamlit.')
-streamlit.text('After that I added a few lines of code')
+streamlit.text('After that I added a few lines of code, and saw my page update immediately.')
