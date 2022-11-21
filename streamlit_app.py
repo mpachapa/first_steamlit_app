fb920e5f5401b2000e1605455b5c8bbea9ebf9e0
Matthew Pachapa <90224964+mpachapa@users.noreply.github.com>
2022-11-21 13:53:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,4 +26,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 # For Fruityvice API response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
