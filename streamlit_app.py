cd318fabac3a19fc93ddadc40de4cf35f58f014b
Matthew Pachapa <90224964+mpachapa@users.noreply.github.com>
2022-11-21 14:40:21 -0500

    formalized the json

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 # For Fruityvice API response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
 # takes the json information and formalizes it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
