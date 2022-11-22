5d294ec72580aab6b37465bd3abc657e44d00389
Matthew Pachapa <90224964+mpachapa@users.noreply.github.com>
2022-11-22 10:54:41 -0500

    updated fruity api

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 # For Fruityvice API response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # takes the json information and formalizes it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
